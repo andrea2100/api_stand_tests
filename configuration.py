a56259183040a28c1c80a9ab0f26503fc02bbbe5
unknown <andrea_vzbt@hotmail.com>
2024-01-18 17:14:05 -0400

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://60824fce-42fb-47b6-b37c-18fb2044e7b8.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
